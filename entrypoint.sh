014fa7bd71ca3e359cf7aa48a2bea72ffc728b91
tilston1000 <info@stratosphereqa.com>
2026-05-24 18:17:51 +0100

    docker integration 10

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# IMPORTANT: Ensure this file has executable permissions (chmod +x entrypoint.sh) on the host before running in Docker with a volume mount.
 set -e
 dotnet test --verbosity normal
 allure generate allure-results --clean -o allure-report
