6b5d21a9f2e5a8803a96d1725e9f5f169d48e3a5
tilston1000 <info@stratosphereqa.com>
2026-05-24 20:26:30 +0100

    docker integration 22

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,6 @@ set -e
 # Ensure allure-results directory always exists
 mkdir -p allure-results
 dotnet test --verbosity normal
+echo "Listing /app/allure-results after test run:"
+ls -l /app/allure-results || echo "/app/allure-results not found"
 allure generate allure-results --clean -o allure-report
