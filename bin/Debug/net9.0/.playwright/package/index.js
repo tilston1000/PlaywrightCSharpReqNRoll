9fd81bfa4ffbf60f123edc06bea57d0817e49a06
tilston1000 <info@stratosphereqa.com>
2026-05-26 23:52:26 +0100

    get reporting to work 5

--- bin/Debug/net9.0/.playwright/package/index.js
+++ bin/Debug/net9.0/.playwright/package/index.js
@@ -13,13 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const minimumMajorNodeVersion = 14;
+const minimumMajorNodeVersion = 18;
 const currentNodeVersion = process.versions.node;
 const semver = currentNodeVersion.split('.');
 const [major] = [+semver[0]];
 
 if (major < minimumMajorNodeVersion) {
-  // eslint-disable-next-line no-console
   console.error(
       'You are running Node.js ' +
       currentNodeVersion +
@@ -30,4 +29,4 @@ if (major < minimumMajorNodeVersion) {
   process.exit(1);
 }
 
-module.exports = require('./lib/inprocess');
+module.exports = require('./lib/coreBundle').inprocess.playwright;
